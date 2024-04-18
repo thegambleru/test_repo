b222d5bbfcda146e7f84d9422059e0e53c7f0d21
Albert <Albert>
2024-04-18 14:26:13 +0300

    muie

--- carti_de_joc.py
+++ carti_de_joc.py
@@ -1,2 +1,8 @@
 carti = range(1,14)
 simboluri = ["Pica","Inima","Trefla","Romb"]
+pachet = []
+for carte in carti:
+    for simbol in simboluri: 
+        pachet.append(f"{carte} de {simbol}")
+
+print(pachet)
